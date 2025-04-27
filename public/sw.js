45759321be6d7c0fb63ede4a09a1c38d0d3b7eab
Satyajeet Nigade <satyajeetnigade@outlook.com>
2025-04-27 22:33:00 +0530

    Added offline mode to app

---
+++ public/sw.js
@@ -0,0 +1,59 @@
+// This will be injected by the Vite PWA plugin
+self.__WB_MANIFEST
+
+const CACHE_NAME = 'bill-splitter-v1';
+
+// Cache core app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        // Add other static assets that don't change with each build
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Network first, falling back to cache strategy
+self.addEventListener('fetch', (event) => {
+  // Skip cross-origin requests
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // If we got a valid response, clone it and cache it
+        if (response && response.status === 200 && response.type === 'basic') {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        // If network fails, try to serve from cache
+        return caches.match(event.request);
+      })
+  );
+});
